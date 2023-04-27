9d1f1f23bc2c68bbb89871919c3506ae712aecc1
Minsang Song <mssongit@gmail.com>
2023-04-27 14:21:13 +0900

    Update autoencoder-mlp.py

--- autoencoder-mlp.py
+++ autoencoder-mlp.py
@@ -312,3 +312,61 @@ else:
     weight = train['weight'].values
     resp = train['resp'].values
     sw = np.mean(np.abs(train[resp_cols].values), axis = 1)
+    
+    
+n_splits = 5
+group_gap = 31
+
+def create_ae_mlp(num_columns, num_labels, hidden_units, dropout_rates, ls = 1e-2, lr = 1e-3):
+    
+    inp = tf.keras.layers.Input(shape = (num_columns, ))
+    x0 = tf.keras.layers.BatchNormalization()(inp)
+    
+    encoder = tf.keras.layers.GaussianNoise(dropout_rates[0])(x0)
+    encoder = tf.keras.layers.Dense(hidden_units[0])(encoder)
+    encoder = tf.keras.layers.BatchNormalization()(encoder)
+    encoder = tf.keras.layers.Activation('swish')(encoder)
+    
+    decoder = tf.keras.layers.Dropout(dropout_rates[1])(encoder)
+    decoder = tf.keras.layers.Dense(num_columns, name = 'decoder')(decoder)
+    
+    x_ae = tf.keras.layers.Dense(hidden_units[1])(decoder)
+    x_ae = tf.keras.layers.BatchNormalization()(x_ae)
+    x_ae = tf.keras.layers.Activation('swish')(x_ae)
+    x_ae = tf.keras.layers.Dropout(dropout_rates[2])(x_ae)
+    
+    out_ae = tf.keras.layers.Dense(num_labels, activation = 'sigmoid', name = 'ae_action')(x_ae)
+
+    x = tf.keras.layers.Concatenate()([x0, encoder])
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Dropout(dropout_rates[3])(x)
+    
+    for i in range(2, len(hidden_units)):
+        x = tf.keras.layers.Dense(hidden_units[i])(x)
+        x = tf.keras.layers.BatchNormalization()(x)
+        x = tf.keras.layers.Activation('swish')(x)
+        x = tf.keras.layers.Dropout(dropout_rates[i + 2])(x)
+        
+    out = tf.keras.layers.Dense(num_labels, activation = 'sigmoid', name = 'action')(x)
+    
+    model = tf.keras.models.Model(inputs = inp, outputs = [decoder, out_ae, out])
+    model.compile(optimizer = tf.keras.optimizers.Adam(learning_rate = lr),
+                  loss = {'decoder': tf.keras.losses.MeanSquaredError(), 
+                          'ae_action': tf.keras.losses.BinaryCrossentropy(label_smoothing = ls),
+                          'action': tf.keras.losses.BinaryCrossentropy(label_smoothing = ls), 
+                         },
+                  metrics = {'decoder': tf.keras.metrics.MeanAbsoluteError(name = 'MAE'), 
+                             'ae_action': tf.keras.metrics.AUC(name = 'AUC'), 
+                             'action': tf.keras.metrics.AUC(name = 'AUC'), 
+                            }, 
+                 )
+    
+    return model
+
+params = {'num_columns': len(features), 
+          'num_labels': 5, 
+          'hidden_units': [96, 96, 896, 448, 448, 256], 
+          'dropout_rates': [0.03527936123679956, 0.038424974585075086, 0.42409238408801436, 0.10431484318345882, 0.49230389137187497, 0.32024444956111164, 0.2716856145683449, 0.4379233941604448], 
+          'ls': 0, 
+          'lr':1e-3, 
+         }
