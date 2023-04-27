270138d77bb238ea713b2213f444010cdb866786
Minsang Song <mssongit@gmail.com>
2023-04-27 14:19:28 +0900

    Update autoencoder-mlp.py

--- autoencoder-mlp.py
+++ autoencoder-mlp.py
@@ -21,3 +21,16 @@ import tensorflow.keras.layers as layers
 from tensorflow.keras.callbacks import Callback, ReduceLROnPlateau, ModelCheckpoint, EarlyStopping
 
 TEST = False
+
+
+
+# weighted average as per Donate et al.'s formula
+# https://doi.org/10.1016/j.neucom.2012.02.053
+# [0.0625, 0.0625, 0.125, 0.25, 0.5] for 5 fold
+def weighted_average(a):
+    w = []
+    n = len(a)
+    for j in range(1, n + 1):
+        j = 2 if j == 1 else j
+        w.append(1 / (2**(n + 1 - j)))
+    return np.average(a, weights = w)
